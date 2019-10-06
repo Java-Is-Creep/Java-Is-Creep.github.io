ebdfbf3768c51cd4a71481b00bbf0b36b047dbeb
danixx j p <daninafria@hotmail.es>
2019-10-06 20:37:06 +0200

    WIP SPIKES

--- server/src/main/java/server/MapObstacle.java
+++ server/src/main/java/server/MapObstacle.java
@@ -2,9 +2,10 @@ package server;
 
 public class MapObstacle  extends MapObject{
 
+
 	public MapObstacle(int width, int height, int posX, int posY, type myTipe) {
 		super(width, height, posX, posY, myTipe);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
