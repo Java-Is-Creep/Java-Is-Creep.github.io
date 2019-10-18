322cc51a33fdebff67f6f986947eafd1428b072e
David Fontela Moñino <d.fontela.2016@alumnos.urjc.es>
2019-10-18 13:56:02 +0200

    pantalla de error

--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -130,6 +130,7 @@ window.onload = function () {
 
     game.global.socket.onclose = () => {
         console.log('[DEBUG] Websocket connection closed');
+        game.state.start('errorState')
     }
 
     game.global.socket.onmessage = (message) => {
@@ -645,6 +646,7 @@ window.onload = function () {
 
     this.game.state.add('bootState', Slooow.bootState);
     this.game.state.add('preloadState', Slooow.preloadState);
+    this.game.state.add('errorState', Slooow.errorState);
     this.game.state.add('initGameState', Slooow.initGameState);
     this.game.state.add('initSesionState', Slooow.initSesionState);
     this.game.state.add('createAccountState', Slooow.createAccountState);
